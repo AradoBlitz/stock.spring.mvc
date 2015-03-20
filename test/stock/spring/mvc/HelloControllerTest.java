eb1d51363526f59b9ac4a125c55145bbe5e1e037
AradoBlitz <schlachtnavaho@gmail.com>
2015-03-20 21:48:07 +0300

    Sequentaly added ServletDispatcher and its mapping to web.xml. Added spring web context configuration file for provided servlet dispatcher whith controller's definiton. Implemented Controller interface using tdd. Renamed index.jsp to hello.jsp file. Implementation could be started from configuring spring framework or from implementation controllers and business logic if integration to the Spring is known for drveloper.

---
+++ test/stock/spring/mvc/HelloControllerTest.java
@@ -0,0 +1,17 @@
+package stock.spring.mvc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+public class HelloControllerTest {
+
+	@Test
+	public void handleRequestView() throws Exception {
+		HelloController controller = new HelloController();
+		ModelAndView modelAndView = controller.handleRequest(null,null);
+		assertNotNull("Request should be routed by controller",modelAndView);
+		assertEquals("hello.jsp", modelAndView.getViewName());
+	}
+}
