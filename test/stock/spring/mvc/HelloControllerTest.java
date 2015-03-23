f0275a2792196f03be52f1bf8d6a243539b4e228
AradoBlitz <schlachtnavaho@gmail.com>
2015-03-23 14:52:06 +0300

    1.Renamed HelloController to InventoryController. Updated dispatcher-servlet.xml

--- test/stock/spring/mvc/HelloControllerTest.java
+++ test/stock/spring/mvc/HelloControllerTest.java
@@ -5,13 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
-import stock.spring.mvc.web.HelloController;
+import stock.spring.mvc.web.InventoryController;
 
 public class HelloControllerTest {
 
 	@Test
 	public void handleRequestView() throws Exception {
-		HelloController controller = new HelloController();
+		InventoryController controller = new InventoryController();
 		ModelAndView modelAndView = controller.handleRequest(null,null);
 		assertNotNull("Request should be routed by controller",modelAndView);
 		assertEquals("hello", modelAndView.getViewName());
