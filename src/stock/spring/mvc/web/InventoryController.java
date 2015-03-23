16a38075cc876a2e0fd34ec86a945e00e6aea35c
AradoBlitz <schlachtnavaho@gmail.com>
2015-03-23 15:44:17 +0300

    1.Provided InventoryController with reference to ProductManager. 2.Used <c:forEach/> tag to show the Product information. Also replaced title and other text by <fmt:message/> tag which gets its value from message source. 3.Fixed showing renamed inventory page in InventoryController and index.jsp page.

--- src/stock/spring/mvc/web/InventoryController.java
+++ src/stock/spring/mvc/web/InventoryController.java
@@ -1,6 +1,8 @@
 package stock.spring.mvc.web;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,14 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import stock.spring.mvc.service.SimpleProductManager;
+
 public class InventoryController implements Controller{
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest arg0,
 			HttpServletResponse arg1) throws Exception {
 		// TODO Auto-generated method stub
-		
-		return new ModelAndView("hello","now",new Date().toString());
+		Map<String,Object> model = new HashMap<>();
+		model.put("now",new Date().toString());
+		model.put("products", new SimpleProductManager().getProducts());
+		return new ModelAndView("inventory",model);
 	}
 
 }
--- test/stock/spring/mvc/HelloControllerTest.java
+++ test/stock/spring/mvc/HelloControllerTest.java
@@ -2,6 +2,8 @@ package stock.spring.mvc;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,8 +15,9 @@ public class HelloControllerTest {
 	public void handleRequestView() throws Exception {
 		InventoryController controller = new InventoryController();
 		ModelAndView modelAndView = controller.handleRequest(null,null);
-		assertNotNull("Request should be routed by controller",modelAndView);
-		assertEquals("hello", modelAndView.getViewName());
-		assertNotNull("NOW attribute should be provided",(String)modelAndView.getModel().get("now"));
+		assertNotNull("Request should be routed by controller.",modelAndView);
+		assertEquals("inventory", modelAndView.getViewName());
+		assertNotNull("NOW attribute should be provided.",(String)modelAndView.getModel().get("now"));
+		assertNotNull("Products list should be provided.",(List)modelAndView.getModel().get("products"));
 	}
 }
