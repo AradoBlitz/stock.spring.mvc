c86525a0b7b394f78d464e2fe233cf84e7905ead
AradoBlitz <schlachtnavaho@gmail.com>
2015-04-02 02:30:19 +0300

    Implements sumple whith getting data from test's embedded database using single method for creating datasource for @sql and test's @autowired annotation and JdbcTemplate class.

--- test/stock/spring/mvc/database/DatabaseTest.java
+++ test/stock/spring/mvc/database/DatabaseTest.java
@@ -4,11 +4,16 @@ package stock.spring.mvc.database;
 
 import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.jdbc.Sql;
@@ -16,9 +21,11 @@ import org.springframework.test.context.jdbc.SqlScriptsTestExecutionListener;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
+import stock.spring.mvc.domain.Product;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=DatabaseConf.class)              //Неприятность от спринга. Нужно добавлять руками если используешь слистенеры теста. Что ещё потеряно?
+@ContextConfiguration(classes=DatabaseConf.class)              
 @TestExecutionListeners({SqlScriptsTestExecutionListener.class,DependencyInjectionTestExecutionListener.class})
 public class DatabaseTest {
 
@@ -30,5 +37,16 @@ public class DatabaseTest {
 	@Sql({"/test-schema.sql","/load_data.sql"})
 	public void testName() throws Exception {
 		assertNotNull("Datasource should be autowired",dataSource);
+		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+		Product actual = jdbc.queryForObject("select * from products", new RowMapper<Product>(){
+
+			@Override
+			public Product mapRow(ResultSet arg0, int arg1) throws SQLException {
+				
+				return Product.create(arg0.getDouble(3), arg0.getString(2));
+			}
+			
+		});
+		assertEquals(Product.create(5.78, "Lamp"), actual );
 	}
 }
